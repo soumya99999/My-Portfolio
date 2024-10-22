ce44b58a6c1b66ddaeb46daf863c6e29efe2e58d
Soumya Ranjan <soumyabarik7365@gmail.com>
2024-10-23 01:07:54 +0530

    modifying vite

--- temp-proj/vite.config.js
+++ temp-proj/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
@@ -10,11 +9,25 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './public/assets')
+      '@assets': path.resolve(__dirname, './public/assets'),
     }
   },
   server: {
     open: true,
     host: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // This will split large libraries into separate chunks
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      }
+    },
+    // Increase the chunk size warning limit if necessary
+    chunkSizeWarningLimit: 1000, // Set to 1000kB (or adjust to your needs)
   }
-})
+});
