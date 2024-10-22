8cc597980d8a4492248d874714ab46a567a159fb
Soumya Ranjan <soumyabarik7365@gmail.com>
2024-10-22 13:43:02 +0530

    Back

--- temp-proj/vite.config.js
+++ temp-proj/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'temp-proj',
+  base: '/',
   publicDir: 'public',
   resolve: {
     alias: {
