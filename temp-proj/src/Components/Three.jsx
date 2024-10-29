1427cbfbfc80ad26def5a1e598319f26eca173f8
Soumya Ranjan <soumyabarik7365@gmail.com>
2024-10-29 11:16:43 +0530

    updating Three.jsx

--- temp-proj/src/Components/Three.jsx
+++ temp-proj/src/Components/Three.jsx
@@ -3,44 +3,44 @@ import { useGLTF } from '@react-three/drei';
 import gsap from 'gsap';
 
 const Three = ({ position = [0, 0, 0], scale = 5, rotation = [0, 0, 0] }) => {
-  const { nodes, materials } = useGLTF('models/Three.glb');
-  const threeRef = useRef();
-  const [hovered, setHovered] = useState(false);
+    const { nodes, materials } = useGLTF('models/Three.glb');
+    const threeRef = useRef();
+    const [hovered, setHovered] = useState(false);
 
-  // Use GSAP to rotate the model continuously
-  useEffect(() => {
-    if (threeRef.current) {
-      gsap.timeline({ repeat: -1, repeatDelay: 0.5 })
-        .to(threeRef.current.rotation, {
-          y: hovered ? '+=2' : `+=${Math.PI * 2}`,
-          x: hovered ? '+=2' : `-=${Math.PI * 2}`,
-          duration: 2.5,
-          stagger: {
-            each: 0.15,
-          },
-        });
-    }
-  }, [hovered]);
+    // Use GSAP to rotate the model continuously
+    useEffect(() => {
+        if (threeRef.current) {
+            gsap.timeline({ repeat: -1, repeatDelay: 0.5 })
+                .to(threeRef.current.rotation, {
+                    y: hovered ? '+=2' : `+=${Math.PI * 2}`,
+                    x: hovered ? '+=2' : `-=${Math.PI * 2}`,
+                    duration: 5, // Increase duration to slow down the animation
+                    stagger: {
+                        each: 0.15,
+                    },
+                });
+        }
+    }, [hovered]);
 
-  return (
-    <group
-      ref={threeRef}
-      position={position}
-      scale={scale}
-      rotation={rotation}
-      dispose={null}
-      onPointerEnter={() => setHovered(true)}
-      onPointerLeave={() => setHovered(false)}
-    >
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Object_3.geometry}
-        material={materials.initialShadingGroup}
-        position={[-0.284, -0.483, -0.408]}
-      />
-    </group>
-  );
+    return (
+        <group
+            ref={threeRef}
+            position={position}
+            scale={scale}
+            rotation={rotation}
+            dispose={null}
+            onPointerEnter={() => setHovered(true)}
+            onPointerLeave={() => setHovered(false)}
+        >
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Object_3.geometry}
+                material={materials.initialShadingGroup}
+                position={[-0.284, -0.483, -0.408]}
+            />
+        </group>
+    );
 };
 
 useGLTF.preload('models/Three.glb');
